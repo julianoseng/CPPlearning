9384c3a1a85f59e0e64d31135f8760b92c584459
Julian Oseng <julianoseng@protonmail.com>
2022-07-08 15:03:34 -0500

    Arrays learning

---
+++ Arrays/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char vowels[] {'a','e','i','o','u'};
+    cout << "\nThe first vowel is: " << vowels[0] << endl;
+    cout << "The last vowel is: " << vowels[4] << endl;
+    
+    
+    int arr[10] {};
+    arr[0] = 100;
+    arr[10] = 1000;
+    
+    cout << arr[0] << " " << arr[10] << endl;
+    
+    
+    
+    
+    return 0;
+}
